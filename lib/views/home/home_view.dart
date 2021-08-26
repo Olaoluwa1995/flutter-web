116abf78eed0c3a1a2dc58f14304a2de84334755
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-08-26 20:37:03 +0100

    Updated widgets

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
-import 'package:the_basics/views/centered_view/centered_view.dart';
-import 'package:the_basics/views/home/home_content_desktop.dart';
-import 'package:the_basics/views/home/home_content_mobile.dart';
-import 'package:the_basics/widgets/NavigationBar/navigation_bar.dart';
+import 'package:the_basics/widgets/nav_drawer/navigation_drawer.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CenteredView(
-        child: Column(
-          children: <Widget>[
-            const NavigationBar(),
-            Expanded(
-              child: ScreenTypeLayout(
-                mobile: const HomeContentMobile(),
-                desktop: const HomeContentDesktop(),
-              ),
-            )
-          ],
-        ),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? const NavigationDrawer()
+            : null,
       ),
     );
   }
