17b7255f70627096e817ed4610a7bc7c15686cbe
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-08-27 20:38:57 +0100

    Modified screen for responsiveness

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/views/centered_view/centered_view.dart';
+import 'package:the_basics/views/home/home_content_desktop.dart';
+import 'package:the_basics/views/home/home_content_mobile.dart';
 import 'package:the_basics/widgets/nav_drawer/navigation_drawer.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -12,6 +16,20 @@ class HomeView extends StatelessWidget {
         drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
             ? const NavigationDrawer()
             : null,
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[
+              const NavigationBar(),
+              Expanded(
+                child: ScreenTypeLayout(
+                  mobile: const HomeContentMobile(),
+                  desktop: const HomeContentDesktop(),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
